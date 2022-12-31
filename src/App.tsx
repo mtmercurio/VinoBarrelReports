209e98d626670325004f560de3eee12ee39744d6
toreymercurio <mtmercurio@gmail.com>
2022-12-31 14:04:54 -0500

    adding remaining data

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ ChartJS.register(
   Legend
 );
 
-const unitsOfMeasurement = ['milliliters', 'ounces', 'glasses', 'bottles'] as const
+const unitsOfMeasurement = ['ounces', 'milliliters', 'glasses (6oz)', 'bottles (750ml)'] as const
 
 type Total = {
   name: string;
@@ -33,11 +33,13 @@ type Total = {
 
 function App() {
   const [totals, setTotals] = useState<Total[] | []>([])
+  const [remaining, setRemaining] = useState<Total[] | []>([])
   const [totalPoured, setTotalPoured] = useState(0)
   const [totalData, setTotalData] = useState<number[]>([])
+  const [remainingData, setRemainingData] = useState<number[]>([])
   const [totalMoney, setTotalMoney] = useState(0)
   const [timeframeHour, setTimeframeHour] = useState(1)
-  const [unit, setUnit] = useState<typeof unitsOfMeasurement[number]>('milliliters')
+  const [unit, setUnit] = useState<typeof unitsOfMeasurement[number]>('glasses (6oz)')
 
   const shortenLabel = (label: string): string => {
     if (label?.length > 0) {
@@ -59,35 +61,92 @@ function App() {
       case 'milliliters':
         return milliliters
       case 'ounces':
-        return Math.round(milliliters * 0.033814 * 100) / 100
-      case 'glasses':
-        return Math.round((milliliters / 177.441) * 100) / 100
-      case 'bottles':
-        return Math.round((milliliters / 750) * 100) / 100
+        return Math.round(milliliters * 0.033814 * 10) / 10
+      case 'glasses (6oz)':
+        return Math.round(milliliters / 177.441)
+      case 'bottles (750ml)':
+        return Math.round(milliliters / 750)
       default:
         return 0
     }
   }
 
-  const options = {
+  const barRemainingOptions = {
     responsive: true,
     plugins: {
       legend: {
+        display: false,
         position: 'top' as const,
       },
       title: {
         display: true,
-        text: 'Total',
+        text: `Total Remaining (${unit})`,
       },
     },
   };
+
+  const barPouredOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: `Total Poured (${unit})`,
+      },
+    },
+  };
+
+  const piePouredOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: `Total Poured (${unit})`,
+      },
+    },
+  };
+
   const totalPouredLabels = totals?.map(total => shortenLabel(total.name));
   const totalPouredData = {
     labels: totalPouredLabels,
     datasets: [
       {
-        label: 'Total Poured',
+        label: '',
         data: totalData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+      },
+    ],
+  };
+
+  const totalRemainingLabels = remaining?.map(keg => shortenLabel(keg.name));
+  const totalRemainingData = {
+    labels: totalRemainingLabels,
+    datasets: [
+      {
+        label: '',
+        data: remainingData,
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -119,6 +178,23 @@ function App() {
   }, [timeframeHour])
 
   useEffect(() => {
+    //TODO: change this fetch look at the tablet server to get accurate data
+    fetch(`http://${process.env.REACT_APP_SERVER_IP_ADDRESS}:${process.env.REACT_APP_SERVER_API_PORT}/total?timeframeHour=${180}`, {
+      method: 'GET'
+    })
+      .then(async response => await response.json())
+      .then((data: Total[]) => {
+        //TODO: remove all this logic to figure out how much is left
+        let _totalData: Total[] = []
+        data?.forEach(total => {
+          let remaining = 7500 - total.pouredML
+          _totalData.push({...total, pouredML: remaining})
+        })
+        setRemaining(_totalData)
+      })
+  }, [])
+
+  useEffect(() => {
     let _totalPoured = 0
     let _totalMoney = 0
     let _totalData: number[] = []
@@ -133,6 +209,16 @@ function App() {
     setTotalMoney(_totalMoney)
     setTotalData(_totalData)
   }, [totals, unit])
+
+  useEffect(() => {
+    let _totalData: number[] = []
+    let poured = 0;
+    remaining?.forEach(total => {
+      poured = convertMillilitersToUnit(total.pouredML, unit)
+      _totalData.push(poured)
+    })
+    setRemainingData(_totalData)
+  }, [remaining, unit])
 
   const handleUnitChange = (event: SelectChangeEvent) => {
     const unit = event.target.value as typeof unitsOfMeasurement[number]
@@ -182,11 +268,11 @@ function App() {
                 label="Hour"
                 onChange={handleHourChange}
               >
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={8}>8</MenuItem>
-                <MenuItem value={12}>12</MenuItem>
+                <MenuItem value={1}>1 Hour</MenuItem>
+                <MenuItem value={2}>2 Hours</MenuItem>
+                <MenuItem value={4}>4 Hours</MenuItem>
+                <MenuItem value={8}>8 Hours</MenuItem>
+                <MenuItem value={12}>12 Hours</MenuItem>
                 <MenuItem value={24}>Day</MenuItem>
                 <MenuItem value={48}>Two Days</MenuItem>
                 <MenuItem value={168}>Week</MenuItem>
@@ -202,7 +288,7 @@ function App() {
               <Box sx={{textTransform: 'capitalize'}}>Total {unit}</Box>
             </Typography>
             <Typography variant="h5" component="div" align={'center'} gutterBottom>
-              {parseFloat(totalPoured.toFixed(2))}
+              {totalPoured}
             </Typography>
           </Grid>
           <Grid xs={6} sm={3}>
@@ -210,32 +296,30 @@ function App() {
               Total Money
             </Typography>
             <Typography variant="h5" component="div" align={'center'} gutterBottom>
-              ${parseFloat((totalMoney / 100).toFixed(2))}
+              ${totalMoney / 100}
             </Typography>
           </Grid>
-          {totals.length > 0
-            ? <>
-              <Grid xs={12} sm={12} md={12} lg={6}>
-                <Card>
-                  <CardContent>
-                    <Bar options={options} data={totalPouredData}/>
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid xs={12} sm={12} md={12} lg={6}>
-                <Card>
-                  <CardContent>
-                    <Pie data={totalPouredData}/>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </>
-            : <Grid xs={12}>
-              <Typography color="text.secondary" align={'center'} noWrap>
-                No data for selected time
-              </Typography>
-            </Grid>
-          }
+          <Grid xs={12} sm={12} md={12} lg={6}>
+            <Card>
+              <CardContent>
+                <Bar options={barRemainingOptions} data={totalRemainingData}/>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={12} sm={12} md={12} lg={6}>
+            <Card>
+              <CardContent>
+                <Bar options={barPouredOptions} data={totalPouredData}/>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={12} sm={12} md={12} lg={6}>
+            <Card>
+              <CardContent>
+                <Pie options={piePouredOptions} data={totalPouredData}/>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
       </Container>
     </>
